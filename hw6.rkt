231db419ff292dcb37243b60009a334ec1947c62
vsapiens <erick.frank@icloud.com>
2019-09-15 22:57:22 -0500

    Hw6.rkt

--- hw6.rkt
+++ hw6.rkt
@@ -1,21 +1,21 @@
 #lang racket
 
-
 (define (quickSort lst)
   (if (null? lst)
     null
     (append
-      (quickSort (filter (lambda (num) (<= num (car lst))) (cdr lst)))
-      (list (car l))
-      (quickSort (filter (lambda (num) (> num (car lst))) (cdr lst)))
+      (quickSort (filter (lambda (x) (<= x (car lst))) (cdr lst)))
+      (list (car lst))
+      (quickSort (filter (lambda (x) (> x (car lst))) (cdr lst)))
     )
   )
 )
 
-(define (matrMul matrix1 matrix2)
+(define (matrMul mat1 mat2)
   (map
-   (lambda (row) (apply map  (lambda column  (apply + (map * row column) ) matrix2)   )
-   matrix1)
+   (lambda (mult-row) 
+    (apply map (lambda (mult-column) (apply + (map * mult-row mult-column)) mat2) ) mat1)
+  )
 )
 
 ;(matrMul '((1 2 3) (4 5 6) (7 8 9)) '((1 2 3) (4 5 6) (7 8 9)))
@@ -30,16 +30,42 @@
 )
 ;(tableSum '( ("male" 178) ("female" 165) ("female" 158) ("female" 182) ("male" 161) ("male" 175) ("female" 182)("male" 188) ("male" 169) ("female" 156)))
 
-(define (BST)
-  (if (null? BST)
+(define (BST x bst)
+  (if (null? bst)
     (list x '() '())
-    (if (< (car BST) x)
-      (list (car BST) (cadr BST) (insert x (caddr BST)))
-      (list (car BST) (insert x (cadr BST)) (caddr BST))
+    (if (< (car bst) x)
+      (list (car bst) (cadr bst) (BST x (caddr bst)))
+      (list (car bst) (BST x (cadr bst)) (caddr bst))
     )
   )
 )
 
-(define (pathBetween)
-  (display "Not implemented")
-)
+;;=========================NOT WORKING=========================
+;(define (search lst-neighbor b lst-not-neighbor)
+;  (if (null? lst-neighbor)
+;    #f
+
+;    )
+;  )
+;)
+
+;(define (pathBetweenAux a b lst)
+;    (if (null? (filter (lambda (x) (= (car x) a)) lst))
+;      #f
+;      (search (cdar (filter (lambda (x) (= (car x) a)) lst)) b (cdar (filter (lambda (x) (not (= (car x) a))) lst)))
+;    )
+;)
+
+;(define (pathBetween a b lst)
+;  (if (null? lst)
+;   #f
+;   (or 
+;      (pathBetweenAux a b lst)
+;      (pathBetweenAux b a lst)
+;   )
+;  )
+;)
+
+;(pathBetween 1 4 '((1 2 3) (2 3 4) (3 4)))
+;(pathBetween 1 2 '((1 2 3) (2 3 4) (3 4)))
+;(pathBetween 1 2 '((1 2 3) (2 3 4) (3 4)))

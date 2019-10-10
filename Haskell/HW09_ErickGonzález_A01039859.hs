63b3c6193b50cf3f5e1e0c4818635b776ddbc639
vsapiens <erick.frank@icloud.com>
2019-10-09 21:11:40 -0500

    Updated hw09

--- Haskell/HW09_ErickGonzález_A01039859.hs
+++ Haskell/HW09_ErickGonzález_A01039859.hs
@@ -1,13 +1,28 @@
-module HW08 where
+module HW09 where
 --Erick Francisco González Martínez A01039859
 
 -- Matrices
+data Mat = Mat [[Int]] deriving (Show)
 
+matMultiplicationAux :: [[Int]] -> [[Int]] -> [[Int]]
+matMultiplicationAux [] [] = []
+
+matMultiplication :: Mat -> Mat -> Mat
+matMultiplication (Mat x) (Mat y) = Mat (matMultiplicationAux x y)
 
 -- Table summarization
-tableSum::[(Char,Int)]->Int
-tableSum [(x,y)] = y
+averageHeight::[([Char],Int)]->[([Char],Int)]
+averageHeight m = 
+    [("Hombre", sum t1 `div` length t1),("Mujer",sum t2 `div` length t2)]
+    where
+    t1 = map (\(x,y) -> y) $ filter (\(x,y) -> x == "Hombre") m
+    t2 = map (\(x,y) -> y) $ filter (\(x,y) -> x == "Mujer") m
 
 -- List comprehension
+threshhold::[([Char],Int)]-> Int ->[([Char],Int)]
+threshhold [] _ = []
+threshhold m n = filter (\(x,y) -> y <= n) m
 
 --Operations on graphs
+
+data Graph = Graph [ [ Int ] ] deriving (Show)
